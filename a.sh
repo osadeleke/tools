3bfafe20fa245d97b899c27a4b356ddd5da41ce9
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-07 16:59:46 +0000

    add input.sh script

--- a.sh
+++ a.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 git add .
-echo -e "Type your commit message here: \c"
-read commit
-git commit -m "$commit"
+git commit -m "$@"
 git push
---
+++ input.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "First number is $1, second number is $2 and third number is $3"
